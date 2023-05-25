1ca22f93752bb3f5a90f369510a07510a0f1ad48
Javich23 <jchavezchaidez@appdev.yearup.org>
2023-05-25 15:41:01 -0400

    "added custom user greeting"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
     }
 }
